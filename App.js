6d736ce7128d63ac08f7fc8431a125f581babac9
manel kacem <manelkacem11@gmail.com>
2024-11-25 14:28:59 +0100

    Login form

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { HomeScreen } from "./src/screens/HomeScreen";
+import { SignInScreen } from "./src/screens/SignInScreen";
 
 export default function App() {
-  return <HomeScreen />;
+  return <SignInScreen />;
 }
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,91 @@
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+
+export function SignInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/login-svgrepo-com.png")}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Login</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email address"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    marginBottom: 40,
+    fontWeight: "bold",
+    color: "black",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    backgroundColor: "#f1f1f1",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: "100%",
+    flex: 1,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#280D5F",
+  },
+
+  image: {
+    height: "20%",
+    width: "40%",
+  },
+  loginText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
