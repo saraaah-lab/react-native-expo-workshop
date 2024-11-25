a948ac3d86160f0d105cc8643d29eb65cf5836e7
manel kacem <manelkacem11@gmail.com>
2024-11-25 12:22:12 +0100

    Styling WoofPosts using StyleSheet

--- src/components/WoofPosts.js
+++ src/components/WoofPosts.js
@@ -1,38 +1,16 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 export default function WoofPosts(props) {
   return (
-    <View style={{ flexDirection: "row", margin: 12 }}>
-      <Image
-        style={{
-          width: 40,
-          height: 80,
-          borderRadius: 12,
-          flex: 1,
-          marginRight: 10,
-        }}
-        source={{ uri: props.image }}
-      />
-      <View style={{ flex: 2, width: 195 }}>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 12,
-            marginBottom: 2,
-            color: "#280D5F",
-            textTransform: "uppercase",
-          }}
-        >
-          {props.title}
-        </Text>
+    <View style={WoofPostsStyles.container}>
+      <Image style={WoofPostsStyles.image} source={{ uri: props.image }} />
+      <View style={WoofPostsStyles.content}>
+        <Text style={WoofPostsStyles.title}>{props.title}</Text>
         <Text
           numberOfLines={2}
           ellipsizeMode="tail"
-          style={{
-            fontSize: 11,
-            color: "#280D5F",
-          }}
+          style={WoofPostsStyles.description}
         >
           {props.description}
         </Text>
@@ -40,3 +18,32 @@ export default function WoofPosts(props) {
     </View>
   );
 }
+
+const WoofPostsStyles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    margin: 12,
+  },
+  image: {
+    width: 40,
+    height: 80,
+    borderRadius: 12,
+    flex: 1,
+    marginRight: 10,
+  },
+  content: {
+    flex: 2,
+    width: 195,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 12,
+    marginBottom: 2,
+    color: "#280D5F",
+    textTransform: "uppercase",
+  },
+  description: {
+    fontSize: 11,
+    color: "#280D5F",
+  },
+});
