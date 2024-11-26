c709b9734cf7543ed2e6cf80e13b007f57990f68
manel kacem <manelkacem11@gmail.com>
2024-11-26 10:34:39 +0100

    Added new screen for testing

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,10 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "../screens/HomeScreen";
+import { FavoriteScreen } from "../screens/FavoriteScreen";
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="FavoriteScreen" component={FavoriteScreen} />
   </Tab.Navigator>
 );
